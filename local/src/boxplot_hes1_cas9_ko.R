fa82b8d5a1668a0b144b12c1d9d88b081452cb7a
Sofia Borgato <sofia.borgato@unito.it>
2026-07-14 14:39:43 +0000

    modified fig6B

--- local/src/boxplot_hes1_cas9_ko.R
+++ local/src/boxplot_hes1_cas9_ko.R
@@ -2,13 +2,24 @@ library(ggplot2)
 library(ggrepel)
 library(ComplexHeatmap)
 library(circlize)
-genes <- c('HES1','DLL1','ATOH1')
 
-cas9_path<-snakemake@input$cas9_fpkm
-ko_path<-snakemake@input$clones_fpkm
-output_plot<-snakemake@output$outplot
+genes <- c("HES1", "DLL1", "ATOH1")
 
-data <- read.table(cas9_path, stringsAsFactors = FALSE, header = TRUE, row.names = 1)
+cas9_path <- snakemake@input$cas9_fpkm
+ko_path <- snakemake@input$clones_fpkm
+output_plot <- snakemake@output$outplot
+
+
+# =============================================================================
+# CRC0322 Cas9
+# =============================================================================
+
+data <- read.table(
+  cas9_path,
+  stringsAsFactors = FALSE,
+  header = TRUE,
+  row.names = 1
+)
 
 colnames_list <- colnames(data)
 split_names <- strsplit(colnames_list, "_")
@@ -27,13 +38,33 @@ metadata$id <- NULL
 
 data <- data[genes, , drop = FALSE]
 data <- t(data)
-data <- merge(data, metadata, by = "row.names")
+
+data <- merge(
+  data,
+  metadata,
+  by = "row.names"
+)
+
 colnames(data)[1] <- "id"
 
 data$gruppo <- "CRC0322 Cas9"
-data$trattamento <- factor(data$trattamento, levels = c("EGF0.1", "CETUX"))
 
-cloni <- read.table(ko_path, stringsAsFactors = FALSE, header = TRUE, row.names = 1)
+data$trattamento <- factor(
+  data$trattamento,
+  levels = c("EGF0.1", "CETUX")
+)
+
+
+# =============================================================================
+# CRC0322 ATOH1-KO clones
+# =============================================================================
+
+cloni <- read.table(
+  ko_path,
+  stringsAsFactors = FALSE,
+  header = TRUE,
+  row.names = 1
+)
 
 colnames_list <- colnames(cloni)
 split_names <- strsplit(colnames_list, "_")
@@ -52,18 +83,38 @@ metacloni$id <- NULL
 
 cloni <- cloni[genes, , drop = FALSE]
 cloni <- t(cloni)
-cloni <- merge(cloni, metacloni, by = "row.names")
+
+cloni <- merge(
+  cloni,
+  metacloni,
+  by = "row.names"
+)
+
 colnames(cloni)[1] <- "id"
 
 cloni$gruppo <- "CRC0322 KO clones"
-cloni$trattamento <- factor(cloni$trattamento, levels = c("EGF0.1", "CETUX"))
 
+cloni$trattamento <- factor(
+  cloni$trattamento,
+  levels = c("EGF0.1", "CETUX")
+)
+
+
+# =============================================================================
+# Funzione per definire intervalli regolari dell'asse y
+# =============================================================================
 
 nice_breaks_0 <- function(y, k = 5) {
+  
   y_max <- max(y, na.rm = TRUE)
   
   if (!is.finite(y_max) || y_max <= 0) {
-    return(list(breaks = c(0, 1), top = 1))
+    return(
+      list(
+        breaks = c(0, 1),
+        top = 1
+      )
+    )
   }
   
   raw_step <- y_max / k
@@ -76,49 +127,155 @@ nice_breaks_0 <- function(y, k = 5) {
   top <- ceiling(y_max / step) * step
   breaks <- seq(0, top, by = step)
   
-  list(breaks = breaks, top = top)
+  list(
+    breaks = breaks,
+    top = top
+  )
 }
 
-plot_data <- rbind(data, cloni)
-plot_data$gruppo <- factor(plot_data$gruppo, levels = c("CRC0322 Cas9", "CRC0322 KO clones"))
 
-br <- nice_breaks_0(plot_data$HES1, k = 5)
-my_colors <- c(
-  "CRC0322 Cas9"      = "black",
-  "CRC0322 KO clones" = "#3F6FA8"
+# =============================================================================
+# Unione dei dati
+# =============================================================================
+
+plot_data <- rbind(data, cloni)
+
+plot_data$gruppo <- factor(
+  plot_data$gruppo,
+  levels = c(
+    "CRC0322 Cas9",
+    "CRC0322 KO clones"
+  )
 )
-p_unico <- ggplot(plot_data, aes(x = trattamento, y = HES1, color = gruppo)) +
+
+plot_data$trattamento <- factor(
+  plot_data$trattamento,
+  levels = c(
+    "EGF0.1",
+    "CETUX"
+  )
+)
+
+br <- nice_breaks_0(
+  plot_data$HES1,
+  k = 5
+)
+
+
+# =============================================================================
+# Colori associati al trattamento
+# =============================================================================
+
+my_colors <- c(
+  "EGF0.1" = "black",
+  "CETUX" = "red"
+)
+
+
+# =============================================================================
+# Plot
+# =============================================================================
+
+p_unico <- ggplot(
+  plot_data,
+  aes(
+    x = gruppo,
+    y = HES1,
+    color = trattamento,
+    group = interaction(gruppo, trattamento)
+  )
+) +
   geom_boxplot(
     outlier.shape = NA,
-    width = 0.7,
-    position = position_dodge(width = 0.8),
-    alpha = 0.35
+    width = 0.55,
+    fill = "white",
+    position = position_dodge(width = 0.7),
+    show.legend = TRUE
   ) +
   geom_point(
     position = position_jitterdodge(
-      jitter.width = 0.18,
+      jitter.width = 0.12,
       jitter.height = 0,
-      dodge.width = 0.8
+      dodge.width = 0.7
     ),
-    size = 0.3) +
-  scale_fill_manual(values = my_colors) +
-  scale_color_manual(values = my_colors)+
+    size = 0.5,
+    show.legend = FALSE
+  ) +
+  scale_color_manual(
+    values = my_colors,
+    breaks = c("EGF0.1", "CETUX"),
+    labels = c(
+      "EGF0.1" = "EGF 0.1 ng/ml",
+      "CETUX" = "Cetuximab"
+    )
+  ) +
+  scale_x_discrete(
+    labels = c(
+      "CRC0322 Cas9" = "Cas9",
+      "CRC0322 KO clones" = "KO clones"
+    ),
+    expand = expansion(mult = c(0.08, 0.08))
+  ) +
+  scale_y_continuous(
+    limits = c(0, br$top),
+    breaks = br$breaks,
+    expand = c(0, 0)
+  ) +
+  labs(
+    x = NULL,
+    y = "HES1 (FPKM)",
+    color = NULL
+  ) +
+  guides(
+    color = guide_legend(
+      nrow = 1,
+      byrow = TRUE,
+      override.aes = list(
+        fill = "white",
+        linewidth = 0.6
+      )
+    )
+  ) +
   theme_bw() +
   theme(
     text = element_text(size = 8),
     panel.border = element_blank(),
     panel.grid = element_blank(),
     axis.line = element_line(color = "black"),
-    legend.title = element_blank()
-  ) +
-  scale_x_discrete(labels = c("EGF0.1" = "EGF 0.1 ng/ml", "CETUX" = "Cetuximab")) +
-  scale_y_continuous(
-    limits = c(0, br$top),
-    breaks = br$breaks,
-    expand = c(0, 0)
-  ) +
-  labs(x = NULL, y = "HES1 (FPKM)")
+
+    plot.title = element_text(
+      hjust = 0.5,
+      size = 8,
+      margin = margin(b = 0)
+    ),
+
+    legend.position = "top",
+    legend.direction = "horizontal",
+    legend.justification = "center",
+    legend.box.just = "center",
+    legend.key.width = grid::unit(5, "mm"),
+    legend.key.height = grid::unit(4, "mm"),
+    legend.spacing.x = grid::unit(1, "mm"),
+    legend.box.spacing = grid::unit(0, "mm"),
+    legend.margin = margin(0, 0, 0, 0),
+
+    axis.text.x = element_text(size = 8),
+
+    plot.margin = margin(
+      t = 1,
+      r = 2,
+      b = 1,
+      l = 1,
+      unit = "mm"
+    )
+  )
 
 print(p_unico)
-ggsave(output_plot, p_unico, width = 90, height = 55, units = "mm")
 
+ggsave(
+  output_plot,
+  p_unico,
+  width = 55,
+  height = 55,
+  units = "mm"
+)
